1451680304f09dde5e2bb765bba40fd63f112bf6
Chiazor Anthony Offiah <chiazor.offiah@tecnotree.com>
2021-01-13 14:07:39 +0100

    Updated Visit model with relationship to other models

--- app/Visit.php
+++ app/Visit.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Visit extends Model
 {
-    //
+    protected $guarded = [];
+
+    public function link()
+    {
+        return $this->belongsTo(Link::class);
+    }
+    
 }
